cce403817c9e2599d94ad6535a342074c9e2f28e
Grace Lattanzio <44171984+KawaiiAnonymouse@users.noreply.github.com>
2022-01-27 15:26:28 -0600

    docbox

--- assignment00.cc
+++ assignment00.cc
@@ -1,1 +1,9 @@
+/* 	Your Name
+	z1887402
+	CSCI480 - Section 0PE1
+	
+	I certify that this is my own work and where appropriate an extension 
+	of the starter code provided for the assignment.
+*/
 
+cut << "Data Structures are Important!!" << endl;   // Yes, I know there is a typo!!!
